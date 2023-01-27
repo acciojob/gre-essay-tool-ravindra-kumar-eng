02116653783d276aec0c260465ead28a1fddd1da
ravindra-kumar-eng <117750035+ravindra-kumar-eng@users.noreply.github.com>
2023-01-27 12:32:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+var type=document.getElementById("evaluatedText");
+type.addEventListener('input',function updateWordCount(){
+    var text=type.value;
+    var Count=text.trim()? text.trim().split(" ").length:0;
+    document.getElementById("wordCount").innerHTML=Count;
+})
